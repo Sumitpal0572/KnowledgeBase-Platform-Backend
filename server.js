79a64b8b3a37e932c30701508497cfef2a849ba8
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-07-08 12:04:58 +0530

    Db connected successfully

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const app = require("./app")
 const mongoose = require("mongoose")
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 
     .then(() => {
         console.log('MongoDB connected');
         app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
     })
-    .catch(err => console.error('Mnogo DB connection err :', err))
+    .catch(err => console.error('Mongo DB connection err :', err))

71737be87a6ab25d637850f67bb3c6558bcdb35f
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-07-09 21:30:40 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+const documentRoutes = require('./routes/documentRoutes');
+const userRoutes = require('./routes/userRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
 const app = express();
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
 
+app.use('/api/auth', authRoutes);
+app.use('/api/docs', documentRoutes);
+app.use('/api/users', userRoutes);
 
+app.use(errorHandler);
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -17,5 +18,15 @@ const userSchema = new mongoose.Schema({
     }
 
 }, { timestamps: true })
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.matchPassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.Model("User", userSchema)
